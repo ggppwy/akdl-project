51e329ee83a73b7651663a47e8833565bb752b84
dg2026829-source <dg2026829@danggok.hs.kr>
2026-05-22 08:27:50 +0900

    Update maine.py

--- maine.py
+++ maine.py
@@ -8,10 +8,7 @@ A = compound([face, eye1, eye2])
 box(size = vec(10, 1, 10), color = color. green)
 
 
-face = box()
-eye1 = sphere(radius=0.1, pos = vec(0.2,0.2,0.5), color = color.black)
-eye2 = sphere(radius=0.1, pos = vec(-0.2,0.2,0.5), color = color.black)
-A = compound([face, eye1, eye2])
+
 
 while True :
     rate(100)
