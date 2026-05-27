b2bff68e3b9cc4b970e00ce260c2c7f9b3ddbd3f
ggppwy <dg2026804@danggok.hs.kr>
2026-05-27 11:10:33 +0900

    Update maine.py

--- maine.py
+++ maine.py
@@ -48,3 +48,42 @@ dt = 0.01
     BOX.pos = BOX.pos + BOX.v*dt
 
     BOX.v = BOX.v + g*dt
+
+3일차
+ground = box(pos = vec(0,-1,0), size = vec(11.2,1,1), color = color.green)
+
+face = box()
+eye1 = sphere(radius=0.1, pos = vec(0.2,0.2,0.5), color = color.black)
+eye2 = sphere(radius=0.1, pos = vec(-0.2,0.2,0.5), color = color.black)
+
+
+BOX = compound([face, eye1, eye2], pos = vec(0,10,0), color = color.cyan)
+
+g = vec(0,-9.8,0)
+BOX.v = vec(0,0,0)
+dt = 0.01   
+
+# 상자가 지면에 닿는 중심 y 좌표 (지면 표면 -0.5 + 상자 절반 0.5 = 0.0)
+ground_level = 0.0
+
+while True:
+    rate(100)
+    k = keysdown()
+    
+    # [조건 변경] 상자가 공중에 떠 있을 때만(BOX.pos.y > ground_level) 작동합니다.
+    if BOX.pos.y > ground_level:
+        
+        # 1. 공중에 있을 때만 좌우 이동 가능
+        if 'a' in k and BOX.pos.x > -5:
+            BOX.pos.x -= 0.1
+        if 'd' in k and BOX.pos.x < 5:
+            BOX.pos.x += 0.1
+            
+        # 2. 공중에 있으므로 중력 적용 및 하강
+        BOX.v = BOX.v + g * dt
+        BOX.pos = BOX.pos + BOX.v * dt
+
+    else:
+        # 3. 지면에 닿은 순간: 속도를 완전히 0으로 지우고 y축 위치 고정
+        BOX.v = vec(0,0,0)
+        BOX.pos.y = ground_level
